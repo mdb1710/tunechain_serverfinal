26db4ab7be1b64e56d57be34dd3e0d69b9c9038f
mdb1710 <mdb1710@gmail.com>
2019-06-03 08:51:21 -0400

    created playlist service

--- migrations/001.undo.create_tunechain_users.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS moodlist_users;
--- seeds/seed.tunechain_tables.sql
+++ seeds/seed.tunechain_tables.sql
@@ -1,8 +1,6 @@
-INSERT INTO moodlist_users (user_name, password)
+INSERT INTO saved_playlists (id, user_mood, user_genre)
 VALUES
-  ('mdb1710', 'password'),
-  ('dawson_leery',  'director'),
-  ('joey_leery', 'writer'),
-  ('pacey_witter', 'chef'),
-  ('jen_lilly', 'cheerleader');
+  (1, 'sad', 'soul'),
+  (2, 'happy',  'rock');
+  
   
